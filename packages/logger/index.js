9c8841f6f429e52b9be11a467a2d5382b40c5405
josefaidt <josef.aidt@gmail.com>
2019-07-01 09:10:59 -0500

    chore: better lg

--- packages/logger/index.js
+++ packages/logger/index.js
@@ -6,18 +6,9 @@ const time = () => {
   return chalk.gray(timeStr)
 }
 
-const lg = (phrase, ...args) => {
-  if (phrase.length === 1 && phrase[0].length !== 0 && args.length === 0) {
-    return console.log(`${time()} ${phrase}`)
-  } else if (args.length > 0) {
-    let result = ''
-    phrase.forEach((p, i) => {
-      result += p + (args[i] || '')
-    })
-    return console.log(`${time()} ${result}`)
-  } else {
-    throw new Error('Logger requires an input')
-  }
+const lg = (...args) => {
+  args.unshift(`${time()} `)
+  console.log.apply(console, args)
 }
 
 module.exports = lg
