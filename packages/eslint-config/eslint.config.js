1adcc0dac1070dad3a4cd0a0878322f799a6a291
josefaidt <josef.aidt@gmail.com>
2021-01-29 13:25:41 -0600

    2020 -> 2019

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -11,7 +11,7 @@ module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
     parserOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2019,
       sourceType: 'module'
     },
     env: {
